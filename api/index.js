29101fb2dcd0594e3ff49e6c2e2f82c2c906f164
Vivek <vivek.network9525@gmail.com>
2024-02-08 01:14:03 +0530

    create signup api

--- api/index.js
+++ api/index.js
@@ -1,7 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 const app = express();
+app.use(express.json());
+dotenv.config();
 mongoose
   .connect(process.env.MONGOURL)
   .then(() => {
@@ -13,3 +17,6 @@ mongoose
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
