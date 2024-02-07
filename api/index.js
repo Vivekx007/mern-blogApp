c076e4394815cc8c7c40eee9f7e1223393149a24
Vivek <vivek.network9525@gmail.com>
2024-02-08 00:04:32 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 const app = express();
+mongoose
+  .connect(process.env.MONGOURL)
+  .then(() => {
+    console.log("DB connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
