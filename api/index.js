217d2a56bae2a04a7fa5e115045ce4e4b19b305b
Vivek <vivek.network9525@gmail.com>
2024-02-13 21:22:22 +0530

    add user update route

--- api/index.js
+++ api/index.js
@@ -3,8 +3,10 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 dotenv.config();
 mongoose
   .connect(process.env.MONGOURL)
