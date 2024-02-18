f8fe583df544ffc3d5377655ee18d6245326958a
Vivek <vivek.network9525@gmail.com>
2024-02-18 12:14:51 +0530

    create a post api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import postRoutes from "./routes/post.route.js";
 import cookieParser from "cookie-parser";
 const app = express();
 app.use(express.json());
@@ -27,3 +28,4 @@ app.use((err, req, res, next) => {
   const message = err.message || "Internal Server Error";
   res.status(statusCode).json({ success: false, statusCode, message });
 });
+app.use("/api/post", postRoutes);
